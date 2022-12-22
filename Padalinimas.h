2ba78f978073113f724872180c094146e5abcf95
Guoda25 <112932535+Guoda25@users.noreply.github.com>
2022-12-22 17:36:35 +0200

    Add files via upload

---
+++ Padalinimas.h
@@ -0,0 +1,5 @@
+#include "Lib.h"
+
+//void studentu_isskaidymas(vector<Studentas>& studentas, vector<Studentas>& vargsiukai, vector<Studentas>& kietiakai);
+
+void studentu_isskaidymas(vector<Studentas>& studentai, vector<Studentas>& vargsiukai);
